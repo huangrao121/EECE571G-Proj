dbc1cf43de67527a522b850a78d956b83dc6d9c2
Huangrao <huangrao1212@outlook.com>
2023-03-24 17:13:54 -0700

    contract management

---
+++ smart_contract/contracts/Cfmanagement.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./crowdfunding.sol";
+
+contract Cfmanagement{
+    address payable public owner;
+    Crowdfunding[] public Cfs;
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    function createCrowdFunding(string memory title, uint256 duration, uint256 goal) public payable{
+        require(msg.value >= 2, "Not enough deposit");
+        Crowdfunding cf = new Crowdfunding(title,duration,goal);
+        Cfs.push(cf);
+    }
+    
+    function getContractCount() public view returns(uint contractCount){
+        return Cfs.length;
+    }
+}
