c59e38d62cae92590b4603d5741420bc9b2d66a2
chenhongjing <60866283+chenhongjing@users.noreply.github.com>
2023-04-08 16:09:55 -0700

    Deploy smart contract to polygon mumbai

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "polygon_mumbai",
+   networks: {
+      hardhat: {},
+      polygon_mumbai: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
 };
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -4,11 +4,15 @@ async function main() {
   const campaignTitle = "Sample Crowdfunding Campaign";
   const duration = 30 * 24 * 60 * 60; // 30 days in seconds
   const goal = hre.ethers.utils.parseEther("10"); // 10 Ether
+  const min = 0;
+  const description = "Crowdfunding";
 
   const Crowdfunding = await hre.ethers.getContractFactory("Crowdfunding");
-  const crowdfunding = await Crowdfunding.deploy(campaignTitle, duration, goal);
+  const crowdfunding = await Crowdfunding.deploy(campaignTitle, duration, goal, min, description);
 
   await crowdfunding.deployed();
+
+  console.log("Contract deployed to address: ", crowdfunding.address);
 
   console.log(
     `Crowdfunding campaign "${campaignTitle}" with goal ${ethers.utils.formatEther(
