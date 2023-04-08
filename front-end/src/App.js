0a5ddad8821ab09b743fde6845c20b2fc8a56bc6
huang rao <huangrao1212@gmail.com>
2023-04-07 19:11:16 -0700

    Add router to switch the main page and add project page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navigator from './components/navigator/navigator'
+import Create from './components/create/create'
+import Display from './components/display/display'
+import { Route,Switch } from 'react-router-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
   return (
     <div className="App">
       <Navigator/>
+      <Switch>
+        <Route path="/createNewProj" component={Create}/>
+        <Route path="/" component={Display}/>
+      </Switch>
     </div>
   );
 }
--- smart_contract/contracts/Cfmanagement.sol
+++ smart_contract/contracts/Cfmanagement.sol
@@ -9,9 +9,9 @@ contract Cfmanagement{
         owner = payable(msg.sender);
     }
 
-    function createCrowdFunding(string memory title, uint256 duration, uint256 goal) public payable{
+    function createCrowdFunding(string memory title, uint256 duration, uint256 goal, uint min, string memory descript) public payable{
         require(msg.value >= 2, "Not enough deposit");
-        Crowdfunding cf = new Crowdfunding(title,duration,goal);
+        Crowdfunding cf = new Crowdfunding(title,duration,goal,min,descript);
         Cfs.push(cf);
     }
     
